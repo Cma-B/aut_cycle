361cb83311dbd8437721cb7fd9e571f840e0f94f
Sima <sima.boreiri@gmail.com>
2020-06-25 01:35:57 +0200

    corrects the index.html.erb according to the test requirements

--- spec/features/visitor_can_view_articles_spec.rb
+++ spec/features/visitor_can_view_articles_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
-feature ' visitor can view articles'do
+feature ' visitor can view articles' do
     before do 
         visit root_path 
     end
     describe 'visitor can see the articles'do
+
         it 'visitor can see the first article' do 
         expect(page).to have_content 'The unbelievable reality'
         end
 
         it 'visitor can  see the second article'do
-            expect(page).to have_content ' Loosing weight without any diet or workout'
+            expect(page).to have_content 'Loosing weight without any diet or workout'
         end
     end
 end

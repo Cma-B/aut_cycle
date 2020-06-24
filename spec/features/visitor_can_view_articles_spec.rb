45080780c4b5a20cf291c5959bd412b699113742
Sima <sima.boreiri@gmail.com>
2020-06-25 01:00:03 +0200

    adds test to the feature file

--- spec/features/visitor_can_view_articles_spec.rb
+++ spec/features/visitor_can_view_articles_spec.rb
@@ -1,1 +1,15 @@
 require 'rails_helper'
+feature ' visitor can view articles'do
+    before do 
+        visit rout_path 
+    end
+    describe 'visitor can see the articles'do
+        it 'visitor can see the first article' do 
+        expect(page).to have_content 'The unbelievable reality'
+        end
+
+        it 'visitor can  see the second article'do
+            expect(page).to have_content ' Loosing weight without any diet or workout'
+        end
+    end
+end

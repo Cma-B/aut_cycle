d24e6c8b04990e4319455f4641cda51955885f1c
Sima <sima.boreiri@gmail.com>
2020-06-26 00:18:26 +0200

    writes test for dynamic form of web, add model, database

--- spec/features/visitor_can_view_articles_spec.rb
+++ spec/features/visitor_can_view_articles_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 feature ' visitor can view articles' do
     before do 
+        create(:article, title:"The unbelievable reality")
+        create(:article, title:"Loosing weight without any diet or workout")
         visit root_path 
     end
+    
     describe 'visitor can see the articles'do
 
         it 'visitor can see the first article' do 
---
+++ spec/models/article_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Article, type: :model do
+  describe "DB" do
+  it {is_expected.to have_db_column :title }
+  end
+  describe 'validation'do
+  it {is_expected.to validate_presence_of :title}
+  end
+describe 'Factory'do
+  expect(create(:article)).to be_valid
+  end
+end 
+

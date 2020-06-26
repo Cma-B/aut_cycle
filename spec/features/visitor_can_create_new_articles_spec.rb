c0e6f6ee0642fc49e0260e335c8815248c91325e
Sima <sima.boreiri@gmail.com>
2020-06-26 15:46:40 +0200

    makes codes refactored

--- spec/features/visitor_can_create_new_articles_spec.rb
+++ spec/features/visitor_can_create_new_articles_spec.rb
@@ -2,13 +2,16 @@ require 'rails_helper'
 feature 'visitor can write articles'do
     before do 
         visit root_path
+    end
+
+    describe 'visitor can write a article'do
+      before do
         click_on 'Write Article'
         fill_in  'Title', with: 'rails is really fun'
         fill_in  'Content', with: 'good to know that javascrips is so much better'
         click_on 'Create Article'
     end
-
-    describe 'visitor can write a article'do
+    
       it 'visitor should see success message'do
         expect(page).to have_content "Article Created"
       end

8adc4dade49de19c2185032e2b81fc286afb2b6a
Sima <sima.boreiri@gmail.com>
2020-06-28 17:43:46 +0200

    modifies feature tests files

--- spec/features/visitor_can_view_article_content_spec.rb
+++ spec/features/visitor_can_view_article_content_spec.rb
@@ -3,11 +3,12 @@ require 'rails_helper'
 feature ' visitor can view articles content' do
     before do 
         create(:article, title: "google reality", content: "two people and half")
-        visit root_path 
-        click_on "google reality"
+        visit welcome_index_path
+        click_on "My Blog"
+        click_on "Show"
     end
     
-    describe 'visitor can see article content'do
+    describe 'visitor can see article title and content'do
       it 'visitor can see the article title' do 
         expect(page).to have_content 'google reality'
       end
--- spec/features/visitor_can_view_articles_spec.rb
+++ spec/features/visitor_can_view_articles_spec.rb
@@ -6,13 +6,13 @@ feature ' visitor can view articles' do
         visit root_path 
     end
     
-    describe 'visitor can see the articles'do
+    describe 'visitor can see the article 'do
 
-        it 'visitor can see the first article' do 
+        it 'visitor can see the first article title' do 
             expect(page).to have_content 'The unbelievable reality'
         end
 
-        it 'visitor can  see the second article'do
+        it 'visitor can  see the second article title'do
             expect(page).to have_content 'Loosing weight without any diet or workout'
         end
     end

ac8974a242a644a4cb99920cfcaa607b8cafc0e0
Sima <sima.boreiri@gmail.com>
2020-06-28 19:52:02 +0200

    modifies the view feature and passes

--- spec/features/visitor_can_view_articles_spec.rb
+++ spec/features/visitor_can_view_articles_spec.rb
@@ -3,10 +3,11 @@ feature ' visitor can view articles' do
     before do 
         create(:article, title:"The unbelievable reality")
         create(:article, title:"Loosing weight without any diet or workout")
-        visit root_path 
+        visit welcome_index_path
+        click_on 'My Blog'
     end
     
-    describe 'visitor can see the article 'do
+    describe 'visitor can see the article list 'do
 
         it 'visitor can see the first article title' do 
             expect(page).to have_content 'The unbelievable reality'

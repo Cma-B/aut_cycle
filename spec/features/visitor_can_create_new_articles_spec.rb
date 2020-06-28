6dda8ae2a3c7c13cf368f2d66cd00bfd2a8ad481
Sima <sima.boreiri@gmail.com>
2020-06-28 19:59:11 +0200

    modifies the create faeture test and passes

--- spec/features/visitor_can_create_new_articles_spec.rb
+++ spec/features/visitor_can_create_new_articles_spec.rb
@@ -1,19 +1,20 @@
 require 'rails_helper'
 feature 'visitor can write articles'do
     before do 
-        visit root_path
+        visit welcome_index_path
+        click_on 'My Blog'
     end
 
     describe 'visitor can write a article'do
       before do
-        click_on 'Write Article'
+        click_on 'New Article'
         fill_in  'Title', with: 'rails is really fun'
         fill_in  'Content', with: 'good to know that javascrips is so much better'
         click_on 'Create Article'
     end
     
       it 'visitor should see success message'do
-        expect(page).to have_content "Article Created"
+        expect(page).to have_content "Article Created!"
       end
     end
 end

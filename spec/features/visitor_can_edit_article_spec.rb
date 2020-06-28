e1d24458deb1951f0509fe49d32c57eee2764e1f
Sima <sima.boreiri@gmail.com>
2020-06-28 20:03:46 +0200

    modifies edit feature test codes and passes

--- spec/features/visitor_can_edit_article_spec.rb
+++ spec/features/visitor_can_edit_article_spec.rb
@@ -2,16 +2,17 @@ require 'rails_helper'
 feature 'visitor can edit the article'do
   before do 
     create(:article, title: " Nitty Gritty ", content: "The best ever Thai dishes by alien chef from Mars")
-    visit root_path 
-    click_on 'Edit Article'
+    visit welcome_index_path 
+    click_on 'My Blog'
+    
   end
 
   describe 'visitor can update the article ' do
     before do 
-      
+        click_on 'Edit'
         fill_in "Title", with: "Nitty Gritty, Thai buffe"
         fill_in "Content", with: "The best ever Thai dishes with high quality material by alien chef from Mars"
-        click_on "Save changes"
+        click_on "Save Changes"
     end
 
     it 'visitor should see success message'do

bda01a4ea50e925634f4d3e980397730f1889e67
Sima <sima.boreiri@gmail.com>
2020-07-02 00:17:55 +0200

    modifies spec file for creating file

--- spec/features/visitor_can_create_new_article_after_signin_spec.rb
+++ spec/features/visitor_can_create_new_article_after_signin_spec.rb
@@ -1,24 +1,19 @@
 require 'rails_helper'
-  feature 'visitor can write articles if they sign in'do
-    before do 
-      create(:user, email: 'anna@mail.com', password: 'bigbear12')
+  feature 'visitor can write articles after sign in' do
+    let(:user) { FactoryBot.create(:user) }
+    before do
+      login_as(user, scope: :user)
       visit welcome_index_path
       click_on 'My Blog'
-      click_on 'Sign in'
-      fill_in 'Email', with: 'anna@mail.com'
-      fill_in 'Password', with: 'bigbear12'
-    describe 'visitor can create new article' do
-      before do
-      visit welcome_index_path
-      click_on 'My Blog'
-      click_on 'Write Article'
+      click_on 'New Article'
       fill_in 'Title', with: 'Final steps'
       fill_in 'Content', with:  'It would be very hard'
       click_on 'Create Article'
-    end
-    it 'visitor should see success message'do
+     end
+    it 'visitor should see success message' do
       expect(page).to have_content "Article Created!"
     end
   end
-end
-end
+
+
+
